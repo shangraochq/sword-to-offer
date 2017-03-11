c64db3b206d59e0728996cdae5ffd5ceb1c5c111
chq <shangraochq@gmail.com>
2017-03-11 22:11:12 +0800

    add

--- offer题目/c++.cpp
+++ offer题目/c++.cpp
@@ -63,3 +63,166 @@ ListNode * reverseList(ListNode* pHead){
 	}
 	return pReverseHead;
 }
+
+//17合并两个排序的链表
+ListNode* mergeSortList(ListNode* pHead1, ListNode* pHead2) {
+
+	if (pHead1 == NULL)
+	{
+		return pHead2;
+	}
+	else if (pHead2 == NULL)
+	{
+		return pHead1;
+	}
+	ListNode* pMergeHead = NULL;
+	if (pHead1->value >= pHead2->value)
+	{
+		pMergeHead = pHead2;
+		pMergeHead->next = mergeSortList(pHead1, pHead2->next);
+	}
+	else {
+		pMergeHead = pHead1;
+		pMergeHead->next = mergeSortList(pHead1->next, pHead2);
+	}
+	return pMergeHead;
+}
+
+//18树的子结构
+ void isChildStruct(ListNode* pFatherRoot, ListNode* pChildRoot) {
+ 	bool result = false;
+ 	if (pFatherRoot != NULL && pChildRoot != NULL)
+ 	{
+ 		if (pFatherRoot == pChildRoot)
+ 		{
+ 			result = true;
+ 		}
+ 		else {
+ 			isChildStruct(pFatherRoot->left, pChildRoot);
+ 			isChildStruct(pFatherRoot->right, pChildRoot); 
+ 		}
+ 	}
+ 	return result;
+ }
+
+ //19二叉树的镜像
+ void mirrorBinaryTree(BinaryTreeNode* pNode) {
+ 	if (pNode == NULL)
+ 	{
+ 		return;
+ 	}
+
+ 	if (pNode->left == NULL && pNode->right == NULL)
+ 	{	
+ 		return;
+ 	}
+ 	BinaryTreeNode* temp = pNode->left;
+ 	pNode->left = pNode->right;
+ 	pNode->right = temp;
+ 	mirrorBinaryTree(pNode->left);
+ 	mirrorBinaryTree(pNode->right);
+ }
+
+ //20顺时针打印矩阵
+
+
+
+ //23从上往下打印二叉树
+ void printFromTopToBottom(BinaryTreeNode* pRoot){
+ 	if (pRoot == NULL)
+ 	{
+ 		return;
+ 	}
+ 	std::deque <BinaryTreeNode *> dequeTree;
+ 	dequeTree.push_back(pRoot);
+ 	while(dequeTree.size()){
+ 		BinaryTreeNode* pNode = dequeTree.pop_front();
+ 		print(pNode->value);
+ 		if (pNode->left != NULL)
+ 		{
+ 			dequeTree.push_back(pNode->left);
+ 		}
+ 		if (pNode->right != NULL)
+ 		{
+			dequeTree.push_back(pNode->right);
+ 		}
+
+ 	}
+ 	
+ }
+
+ //24二叉搜索树的后序遍历序列
+ bool verifySquenceOfBst(int *squence, int length){
+ 	if (squence == NULL || length <= 0)
+ 	{
+ 		return false;
+ 	}
+ 	int root = squence[length - 1];
+ 	int i;
+ 	for(int i = 0; i < length; i++) {
+ 		if (squence[i] > root) 
+ 		{
+ 			break;
+ 		}
+ 	}
+
+ 	int j = i;
+ 	for(; j< length; j++) {
+ 		if (squence[j] < root)
+ 		{
+ 			return false;
+ 		}
+ 	}
+ 	bool left = true;
+ 	if (i > 0 )
+ 	{
+ 		left = verifySquenceOfBst(squence, i);
+ 	}
+ 	bool right = true;
+ 	if (i < length -1)
+ 	{
+ 		right = verifySquenceOfBst(squence + i, length - i - 1);
+ 	}
+ 	return left&&right;
+ }
+
+
+ //25二叉树和为某一值得路径
+ 
+
+ //26二叉搜索树和双向链表
+
+
+ //31连续子数组的最大和
+ bool isInvalidInput = false;
+int findMax(int * pData, int length) {
+	if (pData == NULL || length <= 0) {
+		isInvalidInput = true;
+		return 0;
+	}
+	int maxSum = pData[0];//记录最大值
+	int currentSum = 0;//记录当前值得和
+	for(int i = 0; i < length; i++) {
+		if (currentSum <= 0)
+		{
+			currentSum = pData[i];
+		}
+		else {
+			currentSum += pData[i];
+		}
+		if (currentSum > maxSum)
+		{
+			maxSum = currentSum;
+		}
+	}
+	isInvalidInput = true;
+	return maxSum;
+}
+
+
+
+
+
+
+
+
--- offer题目/js.js
+++ offer题目/js.js
@@ -53,3 +53,129 @@ function orderOddEven(array) {
 var array = [1,2,-3,4,-5,-6,7,8];
 array = orderOddEven(array);
 console.log(array);
+
+
+//21包含Min函数的栈
+
+
+//23从上往下打印二叉树
+function print(rootNode) {
+	if (rootNode === null) {
+		return;
+	}
+	var array = [];
+	arrar.push(rootNode);
+	while(rootNode.length === 0){
+		var pNode = array.shift();
+		if (pNode.left) {
+			array.push(pNode.left);
+		}
+		if (pNode.right) {
+			array.push(pNode.right);
+		}
+	}
+}
+
+//31连续子数组的最大和
+function findMax(array) {
+	if (Object.prototype.toString.call(array) !== "[object Array]") {
+		return;
+	}
+	var maxSum = array[0],
+		currentSum = 0,
+		length = array.length;
+	if (length == 0) {
+		return;	
+	}
+	for (var i = 0; i <length; i++) {
+		if (currentSum <= 0) {
+			currentSum = array[i];
+		}
+		else {
+			currentSum += array[i];
+		}
+		if (currentSum > maxSum) {
+			maxSum = currentSum;
+		}
+	}
+	console.log(maxSum);
+	// return maxSum;
+}
+//test
+var array1 = [1,-1,3,10,-4,7,2,-5];
+var array2 = [];
+var array3 = [1];
+var array4 = [-1,2,3,-5];
+findMax(array1);
+findMax(array2);
+findMax(array3);
+findMax(array4);
+
+
+
+//34丑数
+function getSomeUglyNumber(count) {
+	if (count <= 0 || (typeof count).toLowerCase() != "number") {
+		return;
+	}
+	var array = [];
+	array[0] = 1;
+	var currentCount = 1,
+		index2 = 0,
+		index3 = 0,
+		index5 = 0;
+	while(currentCount < count) {
+
+		var min = Min(array[index2]*2, array[index3]*3, array[index5]*5);
+		array[currentCount] = min;
+		while(array[index2]*2 <= array[currentCount]) {
+			index2++;
+		}
+		while(array[index3]*3 <= array[currentCount]) {
+			index3++;
+		}
+		while(array[index5]*5 <= array[currentCount]) {
+			index5++;
+		}
+		currentCount++;
+	}
+	return array[count -1];
+
+}
+function Min(num1, num2, num3) {
+	if (num1 > num2) {
+		return num2 > num3 ? num3 : num2;
+	}
+	else {
+		return num1 > num3 ? num3 : num1;
+	}
+}
+
+//35第一个只出现一次的字符
+function FirstNoRepeatChar(string) {
+	if (Object.prototype.toString.call(string) !== "[object String]") {
+		console.log("请输入字符串");
+	}
+	var array = [];
+	for(var i = 0; i < string.length; i++) {
+		if (array[string.charCodeAt(i)] == undefined) {
+			array[string.charCodeAt(i)] = 1;
+			console.log(1);
+		}
+		else {
+			array[string.charCodeAt(i)]++;
+			console.log(2);
+		}
+
+	}
+
+	for(var j = 0; j < string.length; j ++) {
+		if (array[string.charCodeAt(j)] === 1) {
+			return string[j];
+			console.log(3);
+		}
+	}
+}
+
+//
+
